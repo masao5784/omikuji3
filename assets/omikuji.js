06276bd91ccd57de4a8ce9135bd3832114f671f8
masao5784 <bgryota@icloud.com>
2020-11-24 20:44:47 +0900

    いろいろな細かい仕様の改善

--- assets/omikuji.js
+++ assets/omikuji.js
@@ -20,23 +20,29 @@ window.addEventListener('DOMContentLoaded',function() {
     },3000);
     //結果が表示される
     setTimeout(function(){
-      var luckyNumber = null;
+    var luckyNumber = null;
+    result = omikujiResult[luckyNumber];
       var x = Math.random()*100;
       if(x>=0 & x<0.01){
         luckyNumber = 0;
+        $("#omikujiResult").html("<div class='omikujiResultInner0'>" + result + "</div>");
       }
       else if(x>=0.01 & x<1){
         luckyNumber = 1;
+        $("#omikujiResult").html("<div class='omikujiResultInner1'>" + result + "</div>");
       }
+
       else if(x>=1 & x<10){
         luckyNumber = 2;
+        $("#omikujiResult").html("<div class='omikujiResultInner2'>" + result + "</div>");
       }
       else{
         luckyNumber = 3;
+        $("#omikujiResult").html("<div class='omikujiResultInner3'>" + result + "</div>");
       }
-      result = omikujiResult[luckyNumber];
-      $("#omikujiResult").html("<div class='omikujiResultInner'>" + result + "</div>");
-    },5000);
+    } ,5000);
+      
+  
     //リセットする
     setTimeout(function(){
       $("#omikujiResult").html("<img id='omikujiImg' src='assets/omikuji.png'>");
